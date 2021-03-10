09175c866b7255b9390673ffb3cabcd50c4920f7
aneesham773 <61931107+aneesham773@users.noreply.github.com>
2021-03-10 22:56:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 
 <?php
-$server="localhost:3307";
-$user="root";
-$password="";
-$db="bank";
-$con=mysqli_connect($server,$user,$password,$db);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server=$cleardb_url["host"];
+$cleardb_username=$cleardb_url["user"];
+$cleardb_password=$cleardb_url["pass"];
+$cleardb_db=substr($cleardb_url["path"],1);
+$active_group='default';
+$query_builder = TRUE;
+$con= mysqli_connect($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db);
+
 
 ?>
